d45fadf6226e8474b440fe67122e18f242f7eab9
andika98 <121611042@mhs.uqgresik.ac.id>
2023-05-26 16:16:35 +0700

    Menu Kedai

---
+++ js/script.js
@@ -0,0 +1,16 @@
+// Toggle class active
+
+const navbarNav = document.querySelector('.navbar-nav');
+// ketika humberger menu diklik
+document.querySelector('#humberger-menu').onclick = () => {
+    navbarNav.classList.toggle('active');
+};
+
+const humberger = document.querySelector('#humberger-menu');
+
+document.addEventListener('click', function(e){
+    if(!humberger.contains(e.target) && !navbarNav.contains(e.target)){
+        navbarNav.classList.remove('active');
+    }
+    
+});
